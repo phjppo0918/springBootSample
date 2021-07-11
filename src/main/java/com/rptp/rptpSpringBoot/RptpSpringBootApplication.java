f648a2e95e090b389921213bf26cad0ca061cde3
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-07-11 18:22:47 +0900

    spring boot initial setting

---
+++ src/main/java/com/rptp/rptpSpringBoot/RptpSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.rptp.rptpSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RptpSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RptpSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rptp/rptpSpringBoot/RptpSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rptp.rptpSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RptpSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
