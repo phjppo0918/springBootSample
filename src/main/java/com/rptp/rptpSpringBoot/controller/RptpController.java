f985f01b12fc77a30148316d6d552085d9b304be
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-07-11 20:39:14 +0900

    get parameter

--- src/main/java/com/rptp/rptpSpringBoot/controller/RptpController.java
+++ src/main/java/com/rptp/rptpSpringBoot/controller/RptpController.java
@@ -3,6 +3,7 @@ package com.rptp.rptpSpringBoot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RptpController {
@@ -11,5 +12,13 @@ public class RptpController {
     public String hello(Model model) {
         model.addAttribute("data", "hello rptp!!");
         return "hello";
+    }
+
+    @GetMapping("hello-get-parameter")
+    public String helloGetParameter(
+            @RequestParam("name") String name,
+            Model model) {
+        model.addAttribute("getData", name);
+        return "hello-get-parameter";
     }
 }
