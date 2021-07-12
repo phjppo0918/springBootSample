946cc7ccb16f1ea20e5a2f1412cd639e210c7f23
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-07-12 22:27:41 +0900

    MemberService 구현 및 테스트

---
+++ src/main/java/com/rptp/rptpSpringBoot/service/MemberService.java
@@ -0,0 +1,42 @@
+package com.rptp.rptpSpringBoot.service;
+
+import com.rptp.rptpSpringBoot.model.MemberDTO;
+import com.rptp.rptpSpringBoot.repository.MemberRepository;
+import com.rptp.rptpSpringBoot.repository.impliment.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    //화원가입
+    public Long join(MemberDTO memberDTO) {
+        // 같은 이름의 있는 중복 회원 x
+        validateDuplicateMember(memberDTO);
+
+        memberRepository.save(memberDTO);
+        return memberDTO.getId();
+    }
+
+    private void validateDuplicateMember(MemberDTO memberDTO) {
+        memberRepository.findByName(memberDTO.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                });
+    }
+
+    //전체 회원 조회
+    public List<MemberDTO> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // id에 맞는 회원 조회
+    public Optional<MemberDTO> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
