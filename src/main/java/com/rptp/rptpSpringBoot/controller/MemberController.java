7a29f9cf5520dbd19f6747effb98e8a3e6022f47
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-07-13 19:49:44 +0900

    member 등록 기능 구현

--- src/main/java/com/rptp/rptpSpringBoot/controller/MemberController.java
+++ src/main/java/com/rptp/rptpSpringBoot/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.rptp.rptpSpringBoot.controller;
 
+import com.rptp.rptpSpringBoot.model.MemberDTO;
 import com.rptp.rptpSpringBoot.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createJoinForm() {
+        return "members/createJoinForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setName(form.getName());
+
+        memberService.join(memberDTO);
+
+        return "redirect:/";
     }
 }
