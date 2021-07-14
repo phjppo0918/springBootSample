01f05c1d77b9bf25b4ba3b1f33b32bd6eb84cc73
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-07-14 19:55:32 +0900

    JDBC Template 도입

--- src/main/java/com/rptp/rptpSpringBoot/repository/impliment/JdbcMemberRepository.java
+++ src/main/java/com/rptp/rptpSpringBoot/repository/impliment/JdbcMemberRepository.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+//@Repository
 public class JdbcMemberRepository implements MemberRepository {
 
     private final DataSource dataSource;
---
+++ src/main/java/com/rptp/rptpSpringBoot/repository/impliment/JdbcTemplateMemberRepository.java
@@ -0,0 +1,67 @@
+package com.rptp.rptpSpringBoot.repository.impliment;
+
+import com.rptp.rptpSpringBoot.model.MemberDTO;
+import com.rptp.rptpSpringBoot.repository.MemberRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class JdbcTemplateMemberRepository implements MemberRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    // 생성자가 단 한 개인 경우 Autowired 생략 가능
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public MemberDTO save(MemberDTO memberDTO) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", memberDTO.getName());
+        Number key = jdbcInsert.executeAndReturnKey(new
+                MapSqlParameterSource(parameters));
+        memberDTO.setId(key.longValue());
+        return memberDTO;
+    }
+
+    @Override
+    public Optional<MemberDTO> findById(Long id) {
+        List<MemberDTO> result = jdbcTemplate.
+                query("select * from member where id = ?", memberDTORowMapper(), id);
+        return result.stream().findAny();
+    }
+
+    private RowMapper<MemberDTO> memberDTORowMapper() {
+        return (rs, rowNum) -> {
+
+            MemberDTO memberDTO = new MemberDTO();
+            memberDTO.setId(rs.getLong("id"));
+            memberDTO.setName(rs.getString("name"));
+            return memberDTO;
+        };
+    }
+
+    @Override
+    public List<MemberDTO> findAll() {
+        return jdbcTemplate.query("select * from member", memberDTORowMapper());
+    }
+    @Override
+    public Optional<MemberDTO> findByName(String name) {
+        List<MemberDTO> result = jdbcTemplate.query("select * from member where name = ?", memberDTORowMapper(), name);
+        return result.stream().findAny();
+    }
+}
