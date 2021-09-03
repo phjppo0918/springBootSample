40769fc279238d0ea59610af0dc9c2d80636bd81
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-09-03 13:29:13 +0900

    패키지명 오타 수정

--- src/main/java/com/rptp/rptpSpringBoot/common/security/SecurityConfig.java
+++ src/main/java/com/rptp/rptpSpringBoot/common/security/SecurityConfig.java
@@ -1,8 +1,7 @@
-package com.rptp.rptpSpringBoot.common.sequrity;
+package com.rptp.rptpSpringBoot.common.security;
 
 import com.rptp.rptpSpringBoot.core.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
