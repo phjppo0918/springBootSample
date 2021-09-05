4a3336e20b7a2d13edba3b217af61fc8ebd45b7a
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-09-05 23:16:50 +0900

    config 수정

--- src/main/java/com/rptp/rptpSpringBoot/config/security/AppProperties.java
+++ src/main/java/com/rptp/rptpSpringBoot/config/security/AppProperties.java
@@ -1,8 +1,7 @@
 package com.rptp.rptpSpringBoot.config.security;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.ArrayList;
@@ -15,27 +14,16 @@ public class AppProperties {
     private final OAuth2 oauth2 = new OAuth2();
 
     @Getter
-    @RequiredArgsConstructor
+    @Setter
     public static class Auth {
         private String tokenSecret;
         private long tokenExpirationMsec;
-
-        @Builder
-        public Auth(String tokenSecret, long tokenExpirationMsec) {
-            this.tokenSecret = tokenSecret;
-            this.tokenExpirationMsec = tokenExpirationMsec;
-        }
     }
 
     @Getter
-    @RequiredArgsConstructor
+    @Setter
     public static final class OAuth2 {
         private List<String> authorizedRedirectUris = new ArrayList<>();
-
-        public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
-            this.authorizedRedirectUris = authorizedRedirectUris;
-            return this;
-        }
     }
 
 }
--- src/main/java/com/rptp/rptpSpringBoot/config/security/SecurityConfig.java
+++ src/main/java/com/rptp/rptpSpringBoot/config/security/SecurityConfig.java
@@ -7,11 +7,17 @@ import com.rptp.rptpSpringBoot.core.member.domain.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -39,6 +45,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
         return new HttpCookieOAuth2AuthorizationRequestRepository();
     }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
